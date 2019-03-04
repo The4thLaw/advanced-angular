4a2f6abe5027122396cef3aac62f46096fb904be
The4thLaw <The4thLaw@users.noreply.github.com>
2019-03-04 10:30:37 +0100

    Adding behaviour to the BankAccountService, and testing it

--- src/app/services/bank-account.service.spec.ts
+++ src/app/services/bank-account.service.spec.ts
@@ -9,4 +9,41 @@ describe('BankAccountService', () => {
     const service: BankAccountService = TestBed.get(BankAccountService);
     expect(service).toBeTruthy();
   });
+
+  it('should allow depositing on empty accounts, and return the account details account', () => {
+    const service: BankAccountService = TestBed.get(BankAccountService);
+    const returnedAccount = service.deposit('BE1', 100);
+    expect(returnedAccount).toBeTruthy();
+    expect(returnedAccount.id).toEqual('BE1');
+    expect(returnedAccount.amount).toEqual(100);
+  });
+
+  it('should not allow withdrawing from empty accounts', () => {
+    const service: BankAccountService = TestBed.get(BankAccountService);
+    // We can't expect the .withdraw call directly to throw an error,
+    // because it will throw the error and the .toThrowError won't ever be called.
+    // So we wrap it in a function
+    expect(() => service.withdraw('BE1', 100)).toThrowError();
+  });
+
+  it('should not allow depositing or withdrawing negative amounts', () => {
+    const service: BankAccountService = TestBed.get(BankAccountService);
+    service.deposit('BE1', 100);
+    expect(() => service.deposit('BE1', -1)).toThrowError();
+    expect(() => service.withdraw('BE1', -1)).toThrowError();
+  });
+
+  it('should not allow withdrawing an excessive amount', () => {
+    const service: BankAccountService = TestBed.get(BankAccountService);
+    service.deposit('BE1', 100);
+    expect(() => service.withdraw('BE1', 101)).toThrowError();
+  });
+
+  it('should allow withdrawing from existing account, and return the right balance', () => {
+    const service: BankAccountService = TestBed.get(BankAccountService);
+    service.deposit('BE1', 100);
+    const returnedAccount = service.withdraw('BE1', 80);
+    expect(returnedAccount.id).toEqual('BE1');
+    expect(returnedAccount.amount).toEqual(20);
+  });
 });
--- src/app/services/bank-account.service.ts
+++ src/app/services/bank-account.service.ts
@@ -4,6 +4,37 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class BankAccountService {
+  private accounts = {};
 
   constructor() { }
+
+  deposit(accountId: string, amount: number) {
+    if (amount <= 0) {
+      throw new Error('Can only deposit positive amounts');
+    }
+    if (!this.accounts[accountId]) {
+      this.accounts[accountId] = {
+        id: accountId,
+        amount: 0
+      };
+    }
+    const theAccount = this.accounts[accountId];
+    this.accounts[accountId].amount += amount;
+    return theAccount;
+  }
+
+  withdraw(accountId: string, amount: number) {
+    if (amount <= 0) {
+      throw new Error('Can only withdraw positive amounts');
+    }
+    if (typeof(this.accounts[accountId]) === 'undefined') {
+      throw new Error('No such account: ' + accountId);
+    }
+    const theAccount = this.accounts[accountId];
+    if (theAccount.amount <= amount) {
+      throw new Error('Inufficient funds for account ' + accountId);
+    }
+    theAccount.amount -= amount;
+    return theAccount;
+  }
 }
