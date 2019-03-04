52f2403a0dc4982ad3600a3ee34a1119b2b55c90
The4thLaw <The4thLaw@users.noreply.github.com>
2019-03-04 16:59:52 +0100

    Adding a very artifical effect, just for the example. can be tracked in the DevTools.

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { AccountAction, AccountActionType } from './reducers/accounts.actions';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AppEffects {
   constructor(private actions$: Actions) {}
+
+  @Effect()
+  foo$ = this.actions$.pipe(
+    // Only process deposits
+    ofType(AccountActionType.deposit),
+    // For each of them
+    map((action: AccountAction) => {
+      // And then the reducers should be able to process those actions, rather than the original ones
+      if (action.amount > 1000) {
+        return { type: 'DepositError', reason: 'Too much'};
+      }
+      return { type: 'DepositConfirm', amount: action.amount, accountId: action.accountId};
+    })
+  );
 }
