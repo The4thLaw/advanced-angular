35747304248e08f6cd56ca4c248cac8b19d80d8c
The4thLaw <The4thLaw@users.noreply.github.com>
2019-03-05 14:34:07 +0100

    Fixing the LoginComponent for SSR

--- src/app/authors/author-list/author-list.component.ts
+++ src/app/authors/author-list/author-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
 import { AuthorService } from '../services/author.service';
 import { NewsAuthor } from 'src/app/models/news-author';
 
