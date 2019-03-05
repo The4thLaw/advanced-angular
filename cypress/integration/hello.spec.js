196a98d16f92c61c194772969b93248eea8f23df
The4thLaw <The4thLaw@users.noreply.github.com>
2019-03-05 08:10:56 +0100

    Fixed most Cypress tests

--- cypress/integration/hello.spec.js
+++ cypress/integration/hello.spec.js
@@ -13,29 +13,29 @@ context('Hello', () => {
     })
 
     it('can select BE1', () => {
-        cy.get('#bankAccountId').should('be.empty')
+        cy.get('#bankAccountIdInput').should('be.empty')
         cy.get('table > tbody > tr:first-child button').click()
-        cy.get('#bankAccountId').invoke('val').should('eq', 'BE1')
+        cy.get('#bankAccountIdInput').invoke('val').should('eq', 'BE1')
     })
 
     it('can deposit to BE1', () => {
         cy.get('table > tbody > tr:first-child button').click()
-        cy.get('#amountToProcess').type('200')
+        cy.get('#amountToProcessInput').type('200')
         cy.get('#btn-deposit').click()
         cy.get('table > tbody > tr:first-child > td:nth-child(2)').should('have.text', '300 €')
     });
 
     it('can deposit to BE2', () => {
-        cy.get('#bankAccountId').type('BE2')
-        cy.get('#amountToProcess').type('200')
+        cy.get('#bankAccountIdInput').type('BE2')
+        cy.get('#amountToProcessInput').type('200')
         cy.get('#btn-deposit').click()
         cy.get('table > tbody > tr:nth-child(2) > td:first-child').should('have.text', 'BE2')
         cy.get('table > tbody > tr:nth-child(2) > td:nth-child(2)').should('have.text', '200 €')
     });
 
     it('can withdraw from BE1', () => {
-        cy.get('#bankAccountId').type('BE1')
-        cy.get('#amountToProcess').type('50')
+        cy.get('#bankAccountIdInput').type('BE1')
+        cy.get('#amountToProcessInput').type('50')
         cy.get('#btn-withdraw').click()
         cy.get('table > tbody > tr:first-child > td:nth-child(2)').should('have.text', '50 €')
     })
@@ -43,8 +43,8 @@ context('Hello', () => {
     it('shows an error for invalid withdrawal', () => {
         const stub = cy.stub()  
         cy.on('window:alert', stub)
-        cy.get('#bankAccountId').type('BE1')
-        cy.get('#amountToProcess').type('101')
+        cy.get('#bankAccountIdInput').type('BE1')
+        cy.get('#amountToProcessInput').type('101')
         cy.get('#btn-withdraw').click().then(() => {
             expect(stub.getCall(0)).to.be.calledWith('Inufficient funds for account BE1')      
         })
