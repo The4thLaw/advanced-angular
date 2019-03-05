07a1c2993f2f2b558806b4026c46b65fb8e682fa
The4thLaw <The4thLaw@users.noreply.github.com>
2019-03-05 09:24:30 +0100

    Adding a loading pane

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './services/auth.service';
+import { Store, select } from '@ngrx/store';
+import { State } from './reducers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   isLoggedIn$: Observable<boolean>;
+  loadingText: string;
 
   constructor(
     private router: Router,
-    private auth: AuthService
+    private auth: AuthService,
+    private store: Store<State>
   ) {
     this.isLoggedIn$ = this.auth.getLoggedInStatus();
+  }
+
+  ngOnInit() {
+    this.store.pipe(
+      select('loading')
+    ).subscribe(loading => {
+      this.loadingText = loading;
+    });
   }
 
   goHome() {
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -11,18 +11,33 @@ import { accountReducer } from './accounts.reducers';
 import { Accounts } from '../models/accounts';
 import { newsReducer } from './news.reducers';
 import { News } from '../models/news';
+import { NewsActionType } from './news.actions';
 
 export interface State {
   accounts: Accounts;
   news: News[];
+  loading: string;
   // loggedIn: boolean;
 }
 
 export const reducers: ActionReducerMap<State> = {
   accounts: accountReducer,
-  news: newsReducer
+  news: newsReducer,
+  loading: loadingReducer
   /*,
   loggedIn: ...*/
 };
+
+// TODO: put this in its own file
+function loadingReducer(oldState: string, action: Action): string {
+  switch (action.type) {
+    case NewsActionType.LOAD_NEWS:
+      return 'Loading news';
+    case NewsActionType.NEWS_LOADED:
+      return null;
+    default:
+      return oldState;
+  }
+}
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
